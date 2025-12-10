227693080f70745b59e2498f349b3389787ec6ed
Dania Da'seen <daniadaseen@gmail.com>
2025-12-10 22:00:28 +0300

    feat: enhance signup functionality with routing and styling improvements

--- frontend/petsociety/src/app/app.routes.ts
+++ frontend/petsociety/src/app/app.routes.ts
@@ -1,5 +1,7 @@
 import { Routes } from '@angular/router';
 import { Login } from './components/login/login';
+import { Signup } from './components/signup/signup';
 export const routes: Routes = [
 	{ path: 'login', component: Login },
+	{ path: 'signup', component: Signup }
 ];
--- frontend/petsociety/src/app/app.ts
+++ frontend/petsociety/src/app/app.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet, RouterLinkActive } from '@angular/router';
-
+import { RouterOutlet, RouterLinkActive, RouterLink, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLinkActive],
+  imports: [RouterOutlet, RouterLinkActive, RouterLink],
   templateUrl: './app.html'
 })
-export class App {}
+export class App {
+
+  constructor(private router: Router) { }
+}
