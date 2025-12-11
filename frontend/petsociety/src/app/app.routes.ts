bd6581d0b8dfc491ab8924c896a9dbd20f2ae697
Dana shalabieh <danashalabieh@gmail.com>
2025-12-12 01:19:39 +0300

    feat: add home component with hero, services, and about sections
    - Implemented home component with HTML structure for hero, services, and about sections.
    - Added CSS styles for layout, responsiveness, and hover effects on service cards.
    - Created unit tests for home component to ensure proper instantiation.

--- frontend/petsociety/src/app/app.routes.ts
+++ frontend/petsociety/src/app/app.routes.ts
@@ -1,18 +1,10 @@
 import { Routes } from '@angular/router';
 import { Login } from './components/login/login';
 import { Signup } from './components/signup/signup';
-<<<<<<< HEAD
 import{Home}from './components/home/home';
 export const routes: Routes = [
 	{ path: 'login', component: Login },
 	{ path: 'signup', component: Signup },
 	{path:"", redirectTo:"/Home" , pathMatch:"full"},
 	{path:'Home', component:Home}
-=======
-import { PetSocietyPlus } from './components/PetSocietyPlus/pet-society-plus/pet-society-plus';
-export const routes: Routes = [
-	{ path: 'login', component: Login },
-	{ path: 'signup', component: Signup },
-	{path : 'PetSocietyPlus', component:PetSocietyPlus },
->>>>>>> b3306279685c5fc8109495b6fcfc23e8db5e230f
 ];
