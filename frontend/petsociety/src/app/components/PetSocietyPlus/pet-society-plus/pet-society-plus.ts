a30c5d0187ee14ad2e2702637ea495b8ddf17f81
Dania Da'seen <daniadaseen@gmail.com>
2025-12-11 20:56:24 +0300

    feat: add PetSocietyPlus component with template, styles, and tests

---
+++ frontend/petsociety/src/app/components/PetSocietyPlus/pet-society-plus/pet-society-plus.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pet-society-plus',
+  imports: [],
+  templateUrl: './pet-society-plus.html',
+  styleUrl: './pet-society-plus.css',
+})
+export class PetSocietyPlus {
+
+}
