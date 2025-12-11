f4412bbd94adb46d4106d406c2f9835e11e12663
Dana shalabieh <danashalabieh@gmail.com>
2025-12-12 00:19:21 +0300

    save my changes

--- frontend/petsociety/src/app/app.routes.ts
+++ frontend/petsociety/src/app/app.routes.ts
@@ -1,7 +1,10 @@
 import { Routes } from '@angular/router';
 import { Login } from './components/login/login';
 import { Signup } from './components/signup/signup';
+import{Home}from './components/home/home';
 export const routes: Routes = [
 	{ path: 'login', component: Login },
-	{ path: 'signup', component: Signup }
+	{ path: 'signup', component: Signup },
+	{path:"", redirectTo:"/Home" , pathMatch:"full"},
+	{path:'Home', component:Home}
 ];
