b3306279685c5fc8109495b6fcfc23e8db5e230f
Dania Da'seen <daniadaseen@gmail.com>
2025-12-11 23:39:09 +0300

    fix: update styles and structure for consistency across components

--- frontend/petsociety/src/app/app.routes.ts
+++ frontend/petsociety/src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { Login } from './components/login/login';
 import { Signup } from './components/signup/signup';
+import { PetSocietyPlus } from './components/PetSocietyPlus/pet-society-plus/pet-society-plus';
 export const routes: Routes = [
 	{ path: 'login', component: Login },
-	{ path: 'signup', component: Signup }
+	{ path: 'signup', component: Signup },
+	{path : 'PetSocietyPlus', component:PetSocietyPlus },
 ];
