cf73173cf1f0f1857e6c1fb668777c43d28e62e3
Dania Da'seen <daniadaseen@gmail.com>
2025-12-12 15:01:44 +0300

    feat: add route for PetSocietyPlus component

--- frontend/petsociety/src/app/app.routes.ts
+++ frontend/petsociety/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { Login } from './components/login/login';
 import { Signup } from './components/signup/signup';
 import{Home}from './components/home/home';
+import { PetSocietyPlus } from './components/PetSocietyPlus/pet-society-plus/pet-society-plus';
 export const routes: Routes = [
 	{ path: 'login', component: Login },
 	{ path: 'signup', component: Signup },
+	{path: 'PetSocietyPlus', component: PetSocietyPlus},
 	{path:"", redirectTo:"/Home" , pathMatch:"full"},
 	{path:'Home', component:Home}
 ];
