9b6a0c4c5049e8636896a161e4ca88800254655d
Dania Da'seen <daniadaseen@gmail.com>
2025-12-15 20:59:41 +0300

    Refactor login component to enhance form structure and improve accessibility

--- frontend/petsociety/src/app/components/login/login.ts
+++ frontend/petsociety/src/app/components/login/login.ts
@@ -1,16 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { Router } from '@angular/router';		
+
 
 @Component({
-	selector: 'app-login',
-	templateUrl: './login.html',
-	styleUrls: ['./login.css']
+  selector: 'app-login',
+  imports: [ReactiveFormsModule],
+  templateUrl: './login.html',
+  styleUrls: ['./login.css']
 })
-export class Login implements OnInit {
-isSignUpMode: any;
-toggleSignUp(arg0: boolean) {
-throw new Error('Method not implemented.');
-}
-	constructor() { }
+export class Login  {
 
-	ngOnInit(): void { }
+  loginForm = new FormGroup({
+  Username: new FormControl('', Validators.required),
+  Password: new FormControl('', Validators.required)
+});
+
+  constructor(private router: Router) {}
+
+  login() {
+    if (this.loginForm.valid) {
+      this.router.navigate(['/Home']);
+    }
+  }
 }
+
+
