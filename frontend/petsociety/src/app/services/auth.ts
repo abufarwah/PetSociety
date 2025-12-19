b7126f537846c19271bd712c97e49fc3eb112d79
Dana shalabieh <danashalabieh@gmail.com>
2025-12-20 00:24:58 +0300

    Refactor Auth service to improve user state management and streamline login/logout functionality

--- frontend/petsociety/src/app/services/auth.ts
+++ frontend/petsociety/src/app/services/auth.ts
@@ -1,25 +1,30 @@
 import { Injectable } from '@angular/core';
-
+import { BehaviorSubject } from 'rxjs';
 @Injectable({
   providedIn: 'root',
 })
 export class Auth {
-  private userKey = 'user';
+   private userKey = 'user';
+
+  private loggedInSubject = new BehaviorSubject<boolean>(this.hasUser());
+  isLoggedIn$ = this.loggedInSubject.asObservable();
+
+  private hasUser(): boolean {
+    return !!localStorage.getItem(this.userKey);
+  }
 
   login(user: any) {
     localStorage.setItem(this.userKey, JSON.stringify(user));
-  }
-
-  getUser() {
-    return JSON.parse(localStorage.getItem(this.userKey)!);
-  }
-
-  isLoggedIn(): boolean {
-    return !!localStorage.getItem(this.userKey);
+    this.loggedInSubject.next(true); 
   }
 
   logout() {
     localStorage.removeItem(this.userKey);
+    this.loggedInSubject.next(false);
+  }
+
+  getUser() {
+    return JSON.parse(localStorage.getItem(this.userKey)!);
   }
   
 }
