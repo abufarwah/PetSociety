546b1b51a128d071b6259fd89114ee7dc892c799
Dania Da'seen <daniadaseen@gmail.com>
2025-12-20 01:27:35 +0300

    Enhance account button styling and update login/signup forms for improved user experience

--- frontend/petsociety/src/app/components/signup/signup.ts
+++ frontend/petsociety/src/app/components/signup/signup.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { validate } from '@angular/forms/signals';
 
 @Component({
   selector: 'app-signup',
@@ -14,8 +15,8 @@ export class Signup {
   name: new FormControl('', [Validators.required]),
   phone: new FormControl('', [Validators.required, Validators.pattern(/^\+962[0-9]{9}$/)]),
   email: new FormControl('', [Validators.required, Validators.email]),
-  Password: new FormControl('', [Validators.required, Validators.minLength(8)]),
-  password: new FormControl('', [Validators.required, Validators.minLength(8)])
+  Password1: new FormControl('', [Validators.required, Validators.minLength(8)]),
+  password2: new FormControl('', [Validators.required, Validators.minLength(8)])
 });
   constructor(private router: Router) {}
 
