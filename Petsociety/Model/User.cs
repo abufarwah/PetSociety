c7831e80cfa2b03263cb056a192e048ae460544a
Dania Da'seen <daniadaseen@gmail.com>
2026-04-22 23:02:51 +0300

    Add User model with validation; remove WeatherForecast class

---
+++ Petsociety/Model/User.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Petsociety.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; } 
+        [Required]
+        [MaxLength(100)]
+        public string FullName { get; set; } 
+        [Required]
+
+        [EmailAddress]
+        public string Email { get; set; } 
+
+        [Required]
+        public string PasswordHash { get; set; } 
+    }
+}
