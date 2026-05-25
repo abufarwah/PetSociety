c0e699c1ef724fc573d62ed43cb6333271ab6c33
mariam alsarairah <mariamaiman999@gmail.com>
2026-05-25 03:59:28 +0300

    adding LostFoundReport to DBContext

--- Petsociety/Migrations/20260523194150_AddPaymentTable.Designer.cs
+++ Petsociety/Migrations/20260523194150_AddPaymentTable.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Petsociety.Model;
 
@@ -10,9 +11,11 @@ using Petsociety.Model;
 namespace Petsociety.Migrations
 {
     [DbContext(typeof(PetDbContext))]
-    partial class PetDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260523194150_AddPaymentTable")]
+    partial class AddPaymentTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "9.0.6");
